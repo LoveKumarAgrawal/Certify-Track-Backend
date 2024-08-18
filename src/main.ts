a2b61494ce9993509c3e8972bc75ee591f1dd060
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-08-18 13:27:36 +0530

    fixed CORS error

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true
+  });
   app.use(cookieParser());
   await app.listen(3001);
 }
