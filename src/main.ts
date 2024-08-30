5678f2e277a10cd14e432dcf9a71147bd0578ee8
LoveKumarAgrawal <loveagrawal204@gmail.com>
2024-08-30 16:58:34 +0530

    added allowed origin in env

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: `${process.env.ALLOWED_ORIGIN}`,
     credentials: true
   });
   app.use(cookieParser());
